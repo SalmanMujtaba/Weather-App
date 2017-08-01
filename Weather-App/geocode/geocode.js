669b3b2c766f64c7fceabfe03dd6f263f2dbc024
salman mujtaba <salmanmujtaba1793@gmail.com>
2017-08-01 13:30:54 -0400

    removed comments used to output eorrs

--- Weather-App/geocode/geocode.js
+++ Weather-App/geocode/geocode.js
@@ -1,32 +1,27 @@
 const request = require('request');
 
-geocodeAddress = (address, callsalman) =>{
+geocodeAddress = (address, callback) =>{
   var add = encodeURIComponent(address);
 request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+add,
   json: true
 }, (error,response,body) =>  {
   if(error){
-    callsalman('there was some problem');
-  //  console.log('there was some problem');
+    callback('there was some problem');
   }
   else if(body.status ==='ZERO_RESULTS'){
-    callsalman('no result foound');
+    callback('no result foound');
 
-    //console.log('no result foound');
   }
   else if(body.status === 'OK'){
-    callsalman(undefined,{
+    callback(undefined,{
     address:body.results[0].formatted_address,
     Latitude: body.results[0].geometry.location.lat,
     Longitude: body.results[0].geometry.location.lng
   });
 
-  //console.log('Address:'+ body.results[0].formatted_address);
-  //console.log('Latitude:'+ body.results[0].geometry.location.lat);
-  //console.log('Longitude:'+ body.results[0].geometry.location.lng);
-}
-});
+  }
+ });
 };
-//geocode property avilable
+
 module.exports.geocodeAddress = geocodeAddress;
