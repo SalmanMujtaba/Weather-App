0791fc67601e5dd5ac53fb19d6e9120fd2a3d75e
salman mujtaba <salmanmujtaba1793@gmail.com>
2017-08-01 13:29:25 -0400

    removed comments used to output errors

--- Weather-App/tempFetch.js
+++ Weather-App/tempFetch.js
@@ -1,32 +1,25 @@
 const request = require('request');
 
-fetchTemperature = (latitude,longitude, callsalman) =>{
-//  console.log('temp fetch js');
-request({
+fetchTemperature = (latitude,longitude, callback) =>{
+
+  request({
   url: 'https://api.darksky.net/forecast/07bebebfe6abf20c2c4bcf82c08bd431/'+latitude+','+longitude,
   json: true
-}, (error,response,body) =>  {
-  //console.log(response.statusCode);
+  }, (error,response,body) =>  {
+
   if(error || response.statusCode ===400){
-  //  console.log('error k andar');
-    callsalman('there was some problem');
-  //  console.log('there was some problem');
+    callback('there was some problem');
   }
   else if(!error && response.statusCode ===200)
    {
-  //   console.log('agaye');
-     callsalman(undefined, {
+
+     callback(undefined, {
        temperature: body.currently.temperature,
        apparent: body.currently.apparentTemperature
-     });//'Temp:'+ body.currently.temperature);
-  //   console.log(response);
-
+     });
+     
   }
 
-  //console.log('Address:'+ body.results[0].formatted_address);
-  //console.log('Latitude:'+ body.results[0].geometry.location.lat);
-  //console.log('Longitude:'+ body.results[0].geometry.location.lng);
-});
+ });
 };
-//geocode property avilable
 module.exports.fetchTemperature = fetchTemperature;
